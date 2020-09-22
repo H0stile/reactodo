cc636b582ab4be6c711a2f7ec53bc1d3ed7cf683
CharlesWilmart <chwilmart@gmail.com>
2020-09-22 19:01:12 +0200

    Small update

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "./Todo.css";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  ImgIcon,
+} from "@material-ui/core";
+
+// All component files must be named starting by capital letter
 
 function Todo(props) {
   return (
     <div>
-      <li>{props.text}</li>
+      <List>
+        <ListItem>
+          <ListItemAvatar></ListItemAvatar>
+          <ListItemText primary={props.text} secondary="deadline: " />
+        </ListItem>
+      </List>
     </div>
   );
 }
