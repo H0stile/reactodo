4919bfef8891fd58f14a58463206378b408d29ca
CharlesWilmart <chwilmart@gmail.com>
2020-09-23 18:57:24 +0200

    added delete option with icon

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Todo.css";
 import db from "./firebase";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import { Button } from "@material-ui/core";
 import {
   List,
@@ -22,13 +23,11 @@ function Todo(props) {
           <ListItemText primary={props.todo.todo} secondary="deadline: " />{" "}
           {/* First todo is the object, second is the "todo" field in the doc */}
         </ListItem>
-        <Button
+        <DeleteForeverIcon
           onClick={(event) =>
             db.collection("todos").doc(props.todo.id).delete()
           }
-        >
-          DELETE
-        </Button>
+        />
       </List>
     </div>
   );
