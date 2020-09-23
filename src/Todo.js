e539d31ec0190f33b250561600188e12967d034c
CharlesWilmart <chwilmart@gmail.com>
2020-09-23 18:50:39 +0200

    implemented Icons + deployed the site

--- src/Todo.js
+++ src/Todo.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Todo.css";
+import db from "./firebase";
+import { Button } from "@material-ui/core";
 import {
   List,
   ListItem,
@@ -17,8 +19,16 @@ function Todo(props) {
       <List>
         <ListItem>
           <ListItemAvatar></ListItemAvatar>
-          <ListItemText primary={props.text} secondary="deadline: " />
+          <ListItemText primary={props.todo.todo} secondary="deadline: " />{" "}
+          {/* First todo is the object, second is the "todo" field in the doc */}
         </ListItem>
+        <Button
+          onClick={(event) =>
+            db.collection("todos").doc(props.todo.id).delete()
+          }
+        >
+          DELETE
+        </Button>
       </List>
     </div>
   );
